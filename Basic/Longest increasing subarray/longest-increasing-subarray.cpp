d4028ae5b78fa615b89637a5d8aa310beddb1b00
Vivek <94607666+vivekpatidar549@users.noreply.github.com>
2024-02-09 18:41:17 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Longest increasing subarray/longest-increasing-subarray.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+  public:
+    long int lenOfLongIncSubArr(long int arr[], long int n) {
+        long int maxi=1;
+        long int count=0;
+        for(int i=0;i<n-1;i++){
+            if(arr[i]<arr[i+1]){
+                count++;
+                maxi=max(maxi,count+1);
+            }else{
+                count=0;
+            }
+        }
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long int n;
+        cin >> n;
+        long int arr[n], i;
+        for (i = 0; i < n; i++) 
+            cin >> arr[i];
+        Solution obj;
+        cout << obj.lenOfLongIncSubArr(arr, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
