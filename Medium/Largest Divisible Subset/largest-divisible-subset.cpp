ba0c9e88bb4456a4fe730079e34cdfcd8297c217
Vivek <94607666+vivekpatidar549@users.noreply.github.com>
2024-02-09 18:28:17 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Largest Divisible Subset/largest-divisible-subset.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> LargestSubset(int n, vector<int>& nums) {
+        // Code here
+        sort(nums.begin(),nums.end());
+        vector<int>dp(n,1);
+        vector<int>prev(n,-1);
+        int lastchoosenidx=0;
+        int maxlen=1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]%nums[j]==0){
+                    if(dp[i]<dp[j]+1){
+                        dp[i]=dp[j]+1;
+                        prev[i]=j;
+                    }
+                    if(dp[i]>maxlen){
+                        maxlen=dp[i];
+                        lastchoosenidx=i;
+                    }
+
+                }
+            }
+        }
+
+        vector<int>res;
+        while(lastchoosenidx!=-1){
+            res.push_back(nums[lastchoosenidx]);
+            lastchoosenidx=prev[lastchoosenidx];
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr;
+
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            arr.push_back(x);
+        }
+
+        Solution obj;
+        vector<int> res = obj.LargestSubset(n, arr);
+        int isValidSeq = 1, sz = res.size();
+        for (int i = 0; i < sz; i++) {
+            for (int j = i + 1; j < sz; j++) {
+                if ((res[i] % res[j]) == 0 || (res[j] % res[i]) == 0)
+                    continue;
+                else {
+                    isValidSeq = 0;
+                    break;
+                }
+            }
+        }
+        cout << isValidSeq << " " << sz << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
