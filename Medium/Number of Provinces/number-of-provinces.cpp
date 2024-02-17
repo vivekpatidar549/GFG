cd974eeb58948ecb768641696ff90039235a7b53
Vivek <94607666+vivekpatidar549@users.noreply.github.com>
2024-02-17 15:04:35 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of Provinces/number-of-provinces.cpp
+++ Medium/Number of Provinces/number-of-provinces.cpp
@@ -5,39 +5,75 @@ using namespace std;
 
 // } Driver Code Ends
 //User function Template for C++
+class DisjointSet{
+  vector<int>parent,rank,size;
+  public:
+  DisjointSet(int n){
+      parent.resize(n+1);
+      rank.resize(n+1,0);
+      size.resize(n+1,1);
+      for(int i=0;i<=n;i++) parent[i]=i;
+  }
+  
+  int findUpar(int node){
+    if(node==parent[node]){
+      return node;
+    }
+    return parent[node]=findUpar(parent[node]);
+  }
+  void unionByRank(int u, int v){
+      int ulp_u=findUpar(u);
+      int ulp_v=findUpar(v);
+      if(ulp_u==ulp_v) return;
+      if(rank[ulp_u]<rank[ulp_v]){
+        parent[ulp_u]=ulp_v;
+      }
+      else if(rank[ulp_v]<rank[ulp_u]){
+        parent[ulp_v]=ulp_u;
+      }
+      else{
+        parent[ulp_v]=ulp_u;
+        rank[ulp_u]++;
+      }
+    }
+    void unionBySize(int u,int v){
+       int ulp_u=findUpar(u);
+       int ulp_v=findUpar(v);
+       if(ulp_u==ulp_v) return;
+       if(size[ulp_u]<size[ulp_v]){
+         parent[ulp_u]=ulp_v;
+         size[ulp_v]+=size[ulp_u];
+       }
+       else{
+         parent[ulp_v]=ulp_u;
+         size[ulp_u]+=size[ulp_v];
+       }
+     } 
+      
+  
+};
 
 class Solution {
   public:
-    void solve(int i , int visited[],  map<int,list<int>>&maps){
-        visited[i]=1;
-        for(auto it:maps[i]){
-            if(visited[it]==0){
-                solve(it,visited,maps);
-            }
-        }
-        return;
-    }
-    int numProvinces(vector<vector<int>> adj, int n) {
+    int numProvinces(vector<vector<int>> adj, int V) {
         // code here
-        map<int,list<int>>maps;
-        for(int i=0;i<adj.size();i++){
-            for(int j=0;j<adj[i].size();j++){
+        // using disjoint set
+        // count no. of uniqueue ultimate parent
+        // if a node is parent og himself he his boss so we can count that also
+        
+        DisjointSet ds(V);
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
                 if(adj[i][j]==1){
-                    maps[i].push_back(j);
-                    maps[j].push_back(i);
+                    ds.unionBySize(i,j);
                 }
             }
         }
-        int visited[n]={0};
-        int ans=0;
-        for(int i=0;i<n;i++){
-            if(visited[i]==0){
-                solve(i,visited,maps);
-                ans++;
-            }
+        int cnt=0;
+        for(int i=0;i<V;i++){
+            if(ds.findUpar(i)==i)cnt++;
         }
-        return ans;
-        
+        return cnt;
     }
 };
 
